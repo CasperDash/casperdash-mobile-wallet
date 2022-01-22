ce95fa6b676668a39b57fe563bf6902c74447dcd
Thinh Nguyen <thinhnp@ymese.com>
2022-01-22 16:51:17 +0700

    nit

--- index.js
+++ index.js
@@ -2,9 +2,8 @@
  * @format
  */
 import 'react-native-gesture-handler';
-import "./polyfill";
-import {AppRegistry, LogBox} from 'react-native';
-import './shim'; // Import polyfill features
+import './polyfill';
+import { AppRegistry, LogBox } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
 LogBox.ignoreLogs(['new NativeEventEmitter']);
