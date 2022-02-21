9436f32e51d613b38dabd34da6ce3476474b5381
thanhnguyen <tn140797@gmail.com>
2022-02-21 21:33:09 +0700

    [feature/send] update isValidPublicKey, add services for Confirm Send

---
+++ src/utils/hooks/useSigner.js
@@ -0,0 +1,32 @@
+import { useSelector } from 'react-redux';
+import { CONNECTION_TYPES } from '../constants/settings';
+import { getLoginOptions } from '../selectors/user';
+import { signDeployByCasperSigner } from '../services/casperServices';
+import { signDeployByLedger } from '../services/ledgerServices';
+
+/**
+ * Use the signer specified in the login options to sign a deploy.
+ * @returns The signed deploy is being returned.
+ */
+const useSigner = () => {
+	const loginOptions = useSelector(getLoginOptions);
+
+	const sign = async (deploy, mainAccountHex, setAccountHex) => {
+		switch (loginOptions.connectionType) {
+			case CONNECTION_TYPES.ledger: {
+				return await signDeployByLedger(deploy, {
+					publicKey: mainAccountHex,
+					keyIndex: loginOptions.keyIndex,
+				});
+			}
+			case CONNECTION_TYPES.casperSigner:
+				return await signDeployByCasperSigner(deploy, mainAccountHex, setAccountHex);
+			default:
+				throw Error('Can not find signer');
+		}
+	};
+
+	return { sign };
+};
+
+export default useSigner;
---
+++ src/utils/services/userServices.js
@@ -0,0 +1,20 @@
+import { CLPublicKey } from 'casper-js-sdk';
+import { toMotes } from '../helpers/currency';
+import { buildTransferDeploy } from './casperServices';
+
+/**
+ * It builds a transfer deploy.
+ * @param transactionDetail
+ * @returns The transfer deploy.
+ */
+export const getTransferDeploy = (transactionDetail = {}) => {
+	try {
+		const { fromAddress, toAddress, amount, transferId = 0, fee } = transactionDetail;
+		const fromPbKey = CLPublicKey.fromHex(fromAddress);
+		const toPbKey = CLPublicKey.fromHex(toAddress);
+		return buildTransferDeploy(fromPbKey, toPbKey, toMotes(amount), transferId, fee);
+	} catch (error) {
+		console.error(error);
+		throw new Error(`Failed to build transfer deploy.`);
+	}
+};
