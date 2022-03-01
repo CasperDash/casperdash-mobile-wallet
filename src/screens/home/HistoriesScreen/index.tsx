b40699ebaaaa4841894324e858b78a35fb232272
Nguyen Ngoc Thanh <thanhnguyentsbd@gmail.com>
2022-03-01 17:22:51 +0700

    [feature/histories] add hooks, load histories

--- src/screens/home/HistoriesScreen/index.tsx
+++ src/screens/home/HistoriesScreen/index.tsx
@@ -3,7 +3,7 @@ import {View, Text, StyleSheet, ScrollView, Image} from 'react-native';
 import {ScreenProps} from 'navigation/ScreenProps';
 import MainRouter from 'navigation/stack/MainRouter';
 import {useTokenInfo} from 'utils/hooks/useTokensInfo';
-import {useSelector} from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
 import {getDeploysTransfer} from 'utils/selectors/transfer';
 import {getPublicKey} from 'utils/selectors';
 import {CButton, CHeader, CLayout, Col, Row} from 'components';
@@ -12,6 +12,8 @@ import {scale} from 'device';
 import TransactionItem from 'screens/home/HistoriesScreen/components/TransactionItem';
 import NoDataComponent from 'screens/home/HistoriesScreen/components/NoDataComponent';
 import {useNavigation} from '@react-navigation/native';
+import {allActions} from 'redux_manager';
+import {useDeploysWithStatus} from 'utils/hooks/useTransferDeploys';
 
 const STATUS_MAPPING = [
     {value: '', label: 'All'},
@@ -24,27 +26,37 @@ const STATUS_MAPPING = [
 const HistoriesScreen: React.FC<ScreenProps<MainRouter.HISTORIES_SCREEN>> = ({route}) => {
     const {token} = route.params;
     const {navigate} = useNavigation();
-    console.log('token', token);
     const {symbol} = token;
     const [selectedStatus, setSelectedStatus] = useState(STATUS_MAPPING[0].value);
-
+    const dispatch = useDispatch();
     // Selector
-    /*const publicKey = useSelector(getPublicKey);
+    const publicKey = useSelector(getPublicKey);
     const transferList = useDeploysWithStatus({symbol: symbol, publicKey, status: selectedStatus});
-*/
+    console.log('transferList', transferList);
+
     // Function
     const onTransactionClick = (deploy: any) => {
-        navigate(MainRouter.TRANSFER_HISTORY_SCREEN, {deploy});
+        // navigate(MainRouter.TRANSFER_HISTORY_SCREEN, {deploy});
+        dispatch(
+            allActions.home.pushTransferToLocalStorage('02021172744b5e6bdc83a591b75765712e068e5d40a3be8ae360274fb26503b4ad38', {
+                address: 'CSPR',
+                amount: 2.5,
+                decimals: undefined,
+                deployHash: '86a9a699af58aa37f591eb1c86a3e88ee9b097f61d7e43987ce59c0de51f38d4',
+                fee: 0.1,
+                fromAddress: '02021172744b5e6bdc83a591b75765712e068e5d40a3be8ae360274fb26503b4ad38',
+                status: 'pending',
+                symbol: 'CSPR',
+                timestamp: '2022-02-25T14:48:04.274Z',
+                toAddress: '02021172744b5e6bdc83a591b75765712e068e5d40a3be8ae360274fb26503b4ad38',
+                transferId: '',
+            }),
+        );
     };
-
-    const { tokenInfoByAddress: tokenInfo } = useTokenInfo(token);
-    const publicKey = useSelector(getPublicKey);
-    // @ts-ignore
-    const listTransfers = useSelector((state: any) => getDeploysTransfer(state, {symbol: token.symbol, publicKey, status: selectedStatus}));
-
 
     return (
         <CLayout bgColor={colors.cF8F8F8}
+                 edges={['right', 'top', 'left']}
                  statusBgColor={colors.cF8F8F8}>
             <CHeader title={symbol} style={{backgroundColor: colors.cF8F8F8}}/>
             <Col
@@ -67,6 +79,7 @@ const HistoriesScreen: React.FC<ScreenProps<MainRouter.HISTORIES_SCREEN>> = ({ro
                         }
                     </Row>
                 </ScrollView>
+
                 <TransactionItem onPress={onTransactionClick} value={{}}/>
                 <NoDataComponent/>
             </Col>
--- src/screens/home/TransferHistoryScreen/index.tsx
+++ src/screens/home/TransferHistoryScreen/index.tsx
@@ -1,16 +1,93 @@
 import React, {useState} from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, ScrollView} from 'react-native';
+import {colors, IconCopy, textStyles} from 'assets';
+import {CButton, CHeader, CLayout, Col, Row} from 'components';
+import {ScreenProps} from 'navigation/ScreenProps';
+import MainRouter from 'navigation/stack/MainRouter';
+import {scale} from 'device';
 
-const TransferHistoryScreen = () => {
+// @ts-ignore
+const TransferHistoryScreen: React.FC<ScreenProps<MainRouter.TRANSFER_HISTORY_SCREEN>> = ({route}) => {
+    const {deploy} = route.params;
+    const {symbol} = deploy;
+
     return (
-        <View>
-            <Text>TransferHistoryScreen</Text>
-        </View>
+        <CLayout bgColor={colors.cF8F8F8}
+                 edges={['right', 'top', 'left']}
+                 statusBgColor={colors.cF8F8F8}>
+            <CHeader title={'Send CSRP'} style={{backgroundColor: colors.cF8F8F8}}/>
+            <Col mt={16} style={styles.container}>
+                <ScrollView
+                    showsVerticalScrollIndicator={false}
+                    style={{marginTop: scale(24)}}
+                    contentContainerStyle={styles.contentContainerStyle}>
+                    <Text style={[styles.title, {marginTop: 0}]}>Sending Address</Text>
+                    <CButton>
+                        <Row style={styles.row}>
+                            <Text numberOfLines={1}
+                                  ellipsizeMode={'middle'}
+                                  style={[styles.value, {width: scale(200)}]}>02021172744b5e6bdc83a591b75765712e068e5d40a3be8ae360274fb26503b4ad38</Text>
+                            <IconCopy style={styles.icCopy} width={scale(15)} height={scale(15)}/>
+                        </Row>
+                    </CButton>
+                    <Text style={styles.title}>Receiving Address</Text>
+                    <CButton>
+                        <Row style={styles.row}>
+                            <Text numberOfLines={1}
+                                  ellipsizeMode={'middle'}
+                                  style={[styles.value, {width: scale(200)}]}>02021172744b5e6bdc83a591b75765712e068e5d40a3be8ae360274fb26503b4ad38</Text>
+                            <IconCopy style={styles.icCopy} width={scale(15)} height={scale(15)}/>
+                        </Row>
+                    </CButton>
+                    <Text style={styles.title}>Network Fee</Text>
+                    <Text style={styles.value}>0.00022203 <Text
+                        style={[textStyles.Body1, {fontWeight: '500'}]}>CSPR</Text></Text>
+                    <Text style={styles.title}>Transaction Time</Text>
+                    <Text style={styles.value}>2021-10-19 22:30</Text>
+                    <Text style={styles.title}>Transaction Hash</Text>
+                    <CButton>
+                        <Row style={styles.row}>
+                            <Text style={styles.value}>0xhf797vbco98948nous98c999wrbiosiijdid9wffosf9skcjaf89fcsoch 879b </Text>
+                            <IconCopy style={styles.icCopy} width={scale(15)} height={scale(15)}/>
+                        </Row>
+                    </CButton>
+                    <Text style={styles.title}>Transfer ID</Text>
+                    <Text style={styles.value}>0474938757539385</Text>
+                </ScrollView>
+            </Col>
+        </CLayout>
     );
 };
 
 export default TransferHistoryScreen;
 
 const styles = StyleSheet.create({
-    container: {},
+    container: {
+        width: scale(375),
+        flex: 1,
+        backgroundColor: colors.W1,
+        borderTopLeftRadius: scale(40),
+        borderTopRightRadius: scale(40),
+    },
+    contentContainerStyle: {
+        paddingHorizontal: scale(16),
+    },
+    title: {
+        ...textStyles.Sub1,
+        color: colors.N3,
+        marginTop: scale(20),
+    },
+    value: {
+        ...textStyles.Body1,
+        marginTop: scale(12),
+        paddingRight: scale(20),
+        maxWidth: scale(375 - 32 - 20),
+    },
+    row: {
+        width: scale(375 - 16 * 2),
+        alignItems: 'center',
+    },
+    icCopy: {
+        marginTop: scale(10),
+    },
 });
