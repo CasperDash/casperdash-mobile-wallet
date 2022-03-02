621e1dcda0b9143d94b7ce40a4c6ab8bf76e2e5a
Thinh Nguyen <gaupoit@outlook.com>
2022-03-02 13:33:08 +0700

    Show key

--- src/screens/authentication/create_new_wallet/GetPublicKeyScreen/index.tsx
+++ src/screens/authentication/create_new_wallet/GetPublicKeyScreen/index.tsx
@@ -9,9 +9,13 @@ import {useNavigation} from '@react-navigation/native';
 import {StackNavigationProp} from '@react-navigation/stack';
 import Keys from 'utils/keys';
 import AuthenticationRouter from 'navigation/AuthenticationNavigation/AuthenticationRouter';
-import {getLedgerPublicKey} from 'utils/services/ledgerServices';
+import {getLedgerPublicKey, getListKeys} from 'utils/services/ledgerServices';
 import CasperApp from '@zondax/ledger-casper';
 import ChoosePinRouter from 'navigation/ChoosePinNavigation/ChoosePinRouter';
+import Col from 'react-native-col';
+import { ScrollView } from 'react-native-gesture-handler';
+import KeyComponent from '../components/KeyComponent';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 const delay = (ms: number) => new Promise(success => setTimeout(success, ms));
 
 interface Props {
@@ -21,11 +25,14 @@ interface Props {
 
 const GetPublicKeyScreen = ({transport, setTransport}: Props) => {
     const [error, setError] = useState<any>();
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+    const [listKeys, setListKeys] = useState<any>([]);
     const [publicKey, setPublicKey] = useState<any>(null);
 
     const unmountRef = useRef<boolean>(false);
     const dispatch = useDispatch();
     const {replace} = useNavigation<StackNavigationProp<any>>();
+    const insets = useSafeAreaInsets();
 
     useEffect(() => {
         setupFetchAddress();
@@ -47,6 +54,7 @@ const GetPublicKeyScreen = ({transport, setTransport}: Props) => {
 
     const fetchAddress = async () => {
         try {
+            setIsLoading(true);
             const casperApp = new CasperApp(transport);
             const ledgerPublicKey = await getLedgerPublicKey(casperApp);
             if (unmountRef.current) {
@@ -54,9 +62,9 @@ const GetPublicKeyScreen = ({transport, setTransport}: Props) => {
             }
             if (ledgerPublicKey) {
                 setError(null);
-                setPublicKey(ledgerPublicKey);
+                const keys = await getListKeys(casperApp, listKeys.length, 5);
+                setListKeys(keys);
                 unmountRef.current = true;
-                getAccountInformation(ledgerPublicKey);
             }
             else {
                 setTransport(null);
@@ -67,21 +75,30 @@ const GetPublicKeyScreen = ({transport, setTransport}: Props) => {
             }
             setError(err);
             return null;
+        } finally {
+            setIsLoading(false);
         }
     };
 
-    const getAccountInformation = (ledgerPublicKey: string) => {
+    const getAccountInformation = async (key: any) => {
+        const { keyIndex } = key;
+        const casperApp = new CasperApp(transport);
+        const ledgerPublicKey = await getLedgerPublicKey(casperApp, keyIndex);
+        setIsLoading(true);
+        setPublicKey(ledgerPublicKey);
         dispatch(allActions.user.getAccountInformation({publicKey: ledgerPublicKey}, async (err: any, data: any) => {
-            if (data) {
+            const notFundAccount = err && err.message && err.message.includes('ValueNotFound');
+            if (data || notFundAccount) {
                 await Config.saveItem(Keys.ledger, transport.device);
                 const info = {
                     publicKey: ledgerPublicKey,
                     loginOptions: {
                         connectionType: 'ledger',
-                        keyIndex: 0,
+                        keyIndex: keyIndex,
                     },
                 };
                 await Config.saveItem(Keys.casperdash, info);
+                setIsLoading(false);
                 replace(AuthenticationRouter.CHOOSE_PIN, {
                     screen: ChoosePinRouter.CHOOSE_PIN_SCREEN,
                     params: {
@@ -89,23 +106,45 @@ const GetPublicKeyScreen = ({transport, setTransport}: Props) => {
                     },
                 });
             } else {
+                setIsLoading(false);
                 setTransport(null);
                 Config.alertMess(err);
             }
         }));
     };
 
+    const _renderKeys = () => {
+        const height = insets.bottom === 0 ? 0 : insets.bottom + scale(72);
+        return (
+            listKeys && listKeys.length > 0 && <Col style={[styles.listContainer, {paddingBottom: scale(72) + insets.bottom, minHeight: scale(315) + height}]}>
+                {
+                    <>
+                    <Text style={styles.title}>Choose your key</Text>
+                    <ScrollView>
+                        {
+                            listKeys.map((ledgerKey: Array<object>, i: any) => {
+                                return <KeyComponent key={i} onPress={getAccountInformation} value={ledgerKey} />;
+                            })
+                        }
+                    </ScrollView>
+                    </>
+                }
+            </Col>
+        );
+    };
+
     return (
         <View style={styles.ShowAddressScreen}>
-            {!error && <Text style={styles.loading}>Loading your account</Text>}
+            {isLoading && <Text style={styles.loading}>Loading your account</Text>}
             {
-                !publicKey && error &&
+                !isLoading && !publicKey && error &&
                 <Text style={styles.error}>
-                    A problem occurred, make sure to open the Ethereum application
+                    A problem occurred, make sure to open the Casper application
                     on your Ledger Nano X. (
                     {String((error && error.message) || error)})
                 </Text>
             }
+            {!isLoading && _renderKeys()}
         </View>
     );
 };
@@ -113,6 +152,9 @@ const GetPublicKeyScreen = ({transport, setTransport}: Props) => {
 export default GetPublicKeyScreen;
 
 const styles = StyleSheet.create({
+    listContainer: {
+
+    },
     ShowAddressScreen: {
         flex: 1,
         padding: scale(16),
@@ -130,8 +172,7 @@ const styles = StyleSheet.create({
         fontSize: scale(16),
     },
     title: {
-        ...textStyles.H5,
-        fontSize: scale(22),
+        fontSize: scale(16),
         marginBottom: scale(16),
     },
     address: {
