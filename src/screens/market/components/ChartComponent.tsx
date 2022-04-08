5c8f9339cdaea1d07897c101ffdd7c5fb7336d2a
thanhnguyen <tn140797@gmail.com>
2022-04-08 22:23:26 +0700

    [feature/market] update chart component

--- src/screens/market/components/ChartComponent.tsx
+++ src/screens/market/components/ChartComponent.tsx
@@ -1,60 +1,59 @@
-import React, { useState } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-
-import { LineChart, Grid, XAxis, YAxis } from 'react-native-svg-charts';
-import { scale } from 'device';
-import { Row, Col } from 'components';
+import React from 'react';
+import {
+  VictoryChart,
+  VictoryLine,
+  VictoryTheme,
+  VictoryTooltip,
+  VictoryVoronoiContainer,
+} from 'victory-native';
+import { Col } from 'components';
 import moment from 'moment';
-import { colors, fonts } from 'assets';
+import { colors, textStyles } from 'assets';
+import { Text } from 'react-native';
+import { scale } from 'device';
 
-const ChartComponent = ({ data }: any) => {
-  // const points = monotoneCubicInterpolation({ data, range: 80 });
-
-  console.log('points', data);
-
+const ChartComponent = ({ data, onActivated, onDeactivated }: any) => {
   return (
-    <Row style={{ width: '100%', height: scale(180) }}>
-      <Col style={{ width: '90%' }}>
-        <LineChart
-          animate={true}
-          animationDuration={1000}
-          style={{ height: scale(180) }}
-          data={data || []}
-          xAccessor={({ item }: any) => item.x}
-          yAccessor={({ item }: any) => item.y}
-          svg={{ stroke: 'rgb(134, 65, 244)' }}
-          contentInset={{ top: 20, bottom: 20 }}>
-          <Grid />
-        </LineChart>
-        <XAxis
+    <Col.C mt={-20} ml={16}>
+      <VictoryChart
+        height={scale(250)}
+        animate={{
+          onLoad: {
+            duration: 200,
+          },
+        }}
+        containerComponent={
+          <VictoryVoronoiContainer
+            onActivated={onActivated}
+            onDeactivated={onDeactivated}
+            mouseFollowTooltips={true}
+            labels={({ datum }) =>
+              `${moment(datum.x).format('ddd DD MMM YY, HH:mm')} \n ${datum.y}`
+            }
+            labelComponent={
+              <VictoryTooltip
+                style={{
+                  fill: colors.N2,
+                }}
+                constrainToVisibleArea
+              />
+            }
+          />
+        }
+        theme={VictoryTheme.material}>
+        <VictoryLine
+          style={{
+            data: { stroke: colors.R1 },
+          }}
           data={data}
-          numberOfTicks={4}
-          xAccessor={({ item }: any) => item.x}
-          formatLabel={value =>
-            value ? moment(new Date(value)).format('DD MMMM') : ''
-          }
-          svg={styles.svg}
+          scale={{ x: 'time' }}
         />
-      </Col>
-      <YAxis
-        style={{ marginLeft: scale(10) }}
-        data={data}
-        numberOfTicks={10}
-        yAccessor={({ item }: any) => item.y}
-        contentInset={{ top: scale(20), bottom: scale(20) }}
-        svg={styles.svg}
-        formatLabel={value => `${value}`}
-      />
-    </Row>
+      </VictoryChart>
+      <Text style={[textStyles.Sub1, { marginLeft: -scale(16) }]}>
+        Source - Coingecko
+      </Text>
+    </Col.C>
   );
 };
 
 export default ChartComponent;
-
-const styles = StyleSheet.create({
-  svg: {
-    fill: colors.N2,
-    fontSize: scale(10),
-    fontFamily: fonts.Poppins.regular,
-  },
-});
