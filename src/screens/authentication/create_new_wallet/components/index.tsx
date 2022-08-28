9270c9f77a67477203d7b217a6c2a66c48831896
Liam <hoailinh1210@gmail.com>
2022-08-28 14:42:58 +0700

    feature[account-management]: add dropdown to chose encryption type

--- src/screens/authentication/create_new_wallet/components/index.tsx
+++ src/screens/authentication/create_new_wallet/components/index.tsx
@@ -2,3 +2,5 @@ export { default as PhraseItem } from './PhraseItem';
 export { default as CheckItem } from './CheckItem';
 export { default as ListItem } from './ListItem';
 export { default as DeviceItem } from './DeviceItem';
+export { default as DropdownItem } from './DropdownItem';
+export { default as SelectDropdownComponent } from './SelectDropdownComponent';
