f80bcb1c222dcf536ec47b0686f119d3f505f2ac
KienNguyen <kiennguyen@Kiens-MacBook-Pro.local>
2022-09-08 15:19:56 +0700

    loading account screen

---
+++ src/screens/authentication/create_new_wallet/InitAccountScreen/index.tsx
@@ -0,0 +1,138 @@
+import React, { useCallback, useEffect } from 'react';
+import { Text, StyleSheet, View, ActivityIndicator, Image } from 'react-native';
+import { ScreenProps } from 'navigation/ScreenProps';
+import CreateNewWalletRouter from 'navigation/CreateNewWalletNavigation/CreateNewWalletRouter';
+import { CLayout, Col } from 'components';
+import { colors, fonts, textStyles } from 'assets';
+import { scale } from 'device';
+// @ts-ignore
+import { CommonActions, useNavigation } from '@react-navigation/native';
+import { StackNavigationProp } from '@react-navigation/stack';
+import { Config, Keys } from 'utils';
+import { useDispatch } from 'react-redux';
+import { allActions } from 'redux_manager';
+import { MessageType } from 'components/CMessge/types';
+import { IWallet } from 'casper-storage';
+import { WalletDescriptor } from 'casper-storage/dist/tsc/user/wallet-info';
+import { IHDKey } from 'casper-storage/src/bips/bip32';
+import { CONNECTION_TYPES, WalletType } from 'utils/constants/settings';
+import { createNewUserWithHdWallet } from 'utils/helpers/account';
+import { images } from 'assets';
+
+const InitAccountScreen: React.FC<
+  // @ts-ignore
+  ScreenProps<CreateNewWalletRouter.INIT_ACCOUNT_SCREEN>
+> = ({ route }) => {
+  const { pin, phrases, algorithm } = route.params;
+  const navigation = useNavigation<StackNavigationProp<any>>();
+  const dispatch = useDispatch();
+
+  const setupUser = useCallback(async () => {
+    try {
+      const user = createNewUserWithHdWallet(pin, phrases, algorithm);
+      const acc0: IWallet<IHDKey> = await user.getWalletAccount(0);
+      user.setWalletInfo(
+        acc0.getReferenceKey(),
+        new WalletDescriptor('Account 1'),
+      );
+      const publicKey = await acc0.getPublicKey();
+      const hashingOptions = user.getPasswordHashingOptions();
+      const userInfo = user.serialize();
+      const info = {
+        publicKey: publicKey,
+        loginOptions: {
+          connectionType: CONNECTION_TYPES.passPhase,
+          hashingOptions: hashingOptions,
+        },
+        userInfo: userInfo,
+      };
+      await Config.saveItem(Keys.casperdash, info);
+      await Config.saveItem(Keys.pinCode, pin);
+
+      const wallets = user.getHDWallet()?.derivedWallets || [];
+      const selectedWallet = wallets[0];
+      await Config.saveItem(Keys.selectedWallet, {
+        walletInfo: selectedWallet,
+        publicKey,
+        walletType: WalletType.HDWallet,
+      });
+    } catch (e: any) {
+      const message = {
+        message: e && e.message ? e.message : 'Error',
+        type: MessageType.error,
+      };
+      dispatch(allActions.main.showMessage(message, 10000));
+    }
+  }, [algorithm, dispatch, phrases, pin]);
+
+  useEffect(() => {
+    //Need timeout here to prevent stuck at previous screen
+    setTimeout(() => {
+      setupUser().then(() => {
+        navigation.dispatch(
+          CommonActions.reset({
+            routes: [
+              {
+                name: 'MainStack',
+              },
+            ],
+          }),
+        );
+        dispatch(allActions.main.loadLocalStorage());
+        const message = {
+          message: 'Logged in successfully',
+          type: MessageType.success,
+        };
+        dispatch(allActions.main.showMessage(message, 1000));
+      });
+    }, 100);
+  }, [setupUser, navigation, dispatch]);
+
+  return (
+    <CLayout>
+      <Col style={styles.flex}>
+        <Col.C style={styles.topContainer}>
+          <Image source={images.logo} style={styles.logo} />
+        </Col.C>
+        <Col.T>
+          <Text style={styles.title}>Creating your wallet...</Text>
+        </Col.T>
+        <Col.T mt={78}>
+          <View style={styles.indicator}>
+            <ActivityIndicator size={30} />
+          </View>
+        </Col.T>
+      </Col>
+    </CLayout>
+  );
+};
+
+export default InitAccountScreen;
+
+const styles = StyleSheet.create({
+  topContainer: {
+    width: '100%',
+    height: '40%',
+  },
+  logo: {
+    width: scale(124),
+    height: scale(122),
+  },
+  title: {
+    ...textStyles.Body1,
+    color: colors.c232635,
+    marginBottom: scale(20),
+    fontFamily: fonts.Lato.regular,
+  },
+  indicator: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '100%',
+    height: '100%',
+    position: 'absolute',
+  },
+  flex: {
+    flex: 1,
+  },
+});
