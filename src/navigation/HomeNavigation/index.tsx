dcccc53e9fbb93f75b2103cb52f77ad5b7ca61cd
KienNguyen <kiennguyen@Kiens-MacBook-Pro.local>
2022-09-09 23:59:28 +0700

    auto lock app after 30s

--- src/navigation/HomeNavigation/index.tsx
+++ src/navigation/HomeNavigation/index.tsx
@@ -1,11 +1,58 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
+import { AppState } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { CommonActions, useNavigation } from '@react-navigation/native';
 import { HomeScreens } from './HomeScreens';
 import HomeRouter from './HomeRouter';
-import { createStackNavigator } from '@react-navigation/stack';
+import AuthenticationRouter from 'navigation/AuthenticationNavigation/AuthenticationRouter';
 
 const Stack = createStackNavigator();
 
 const HomeNavigation = () => {
+  const appState = useRef(AppState.currentState);
+  const navigation = useNavigation();
+
+  const resetStack = (name: string) => {
+    navigation.dispatch(
+      CommonActions.reset({
+        routes: [
+          {
+            name: 'AuthenticationStack',
+            state: {
+              routes: [
+                {
+                  name: name,
+                },
+              ],
+            },
+          },
+        ],
+      }),
+    );
+  };
+
+  useEffect(() => {
+    let timeOut: NodeJS.Timeout;
+    const subscription = AppState.addEventListener('change', nextAppState => {
+      if (nextAppState.match(/inactive|background/)) {
+        if (timeOut) {
+          clearTimeout(timeOut);
+        }
+        timeOut = setTimeout(() => {
+          resetStack(AuthenticationRouter.ENTER_PIN);
+        }, 30000);
+      }
+      if (nextAppState === 'active') {
+        clearTimeout(timeOut);
+      }
+      appState.current = nextAppState;
+    });
+
+    return () => {
+      //@ts-ignore
+      subscription.remove();
+    };
+  }, []);
   return (
     <Stack.Navigator
       initialRouteName={HomeRouter.HOME_SCREEN}
