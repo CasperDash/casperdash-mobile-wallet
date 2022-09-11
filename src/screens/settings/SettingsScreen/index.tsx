8b9b6b5ba2bb77941fd957cdcc933a1ef6643467
KienNguyen <kiennguyen@Kiens-MacBook-Pro.local>
2022-09-11 14:48:48 +0700

    extract delete all button to new comp

--- src/screens/settings/SettingsScreen/index.tsx
+++ src/screens/settings/SettingsScreen/index.tsx
@@ -1,30 +1,23 @@
-import React, { useRef, useState } from 'react';
-import { StyleSheet, Linking, Text } from 'react-native';
+import React from 'react';
+import { StyleSheet, Linking } from 'react-native';
 import {
   colors,
   IconAboutUs,
   IconCircleRight,
   IconLock,
   textStyles,
-  fonts,
 } from 'assets';
-import { CAlert, CButton, CHeader, CLayout, Col } from 'components';
+import { CHeader, CLayout, Col } from 'components';
 import { scale } from 'device';
 import { SettingMenu } from 'screens/settings/data';
 import SettingMenuComponent from '../components/SettingMenuComponent';
 import AuthenticationRouter from 'navigation/AuthenticationNavigation/AuthenticationRouter';
-import { Config, Keys } from 'utils';
 import { CASPERDASH_URL } from 'utils/constants/key';
-import { useDispatch } from 'react-redux';
-import { allActions } from 'redux_manager';
-import CConfirmPinModal from 'components/CConfirmPinModal';
 import { useRestack } from 'utils/hooks/useRestack';
 import { StackName } from 'navigation/ScreenProps';
+import DeleteAllDataButton from '../components/DeleteAllDataButton';
 
 function SettingsScreen() {
-  const alertRef = useRef<any>();
-  const dispatch = useDispatch();
-  const [showConfirmPin, setShowConfirmPin] = useState<boolean>(false);
   const reStack = useRestack();
 
   const listMenu: Array<SettingMenu> = [
@@ -54,45 +47,6 @@ function SettingsScreen() {
     resetStack(AuthenticationRouter.ENTER_PIN);
   };
 
-  const onPressNext = () => {
-    alertRef.current.hide();
-    setShowConfirmPin(true);
-  };
-
-  const onDeleteAllData = () => {
-    const alert = {
-      buttonRight: 'Next',
-      alertMessage: (
-        <>
-          <Text style={styles.deleteQuestion}>
-            Are you sure you want to delete your wallet ?
-          </Text>
-          <Text style={styles.deleteMessage}>
-            Your current wallet, accounts and assets will be removed from this
-            app permanently. This action cannot be undone.
-          </Text>
-          <Text style={styles.deleteMessage}>
-            Your can ONLY recover this wallet with your Secret Recovery Phase
-            CasperDash does not have your Secret Recovery Phase.
-          </Text>
-        </>
-      ),
-    };
-    alertRef.current.show(alert);
-  };
-
-  const deleteAllData = () => {
-    Promise.all(
-      Object.entries(Keys).map(key => {
-        return Config.deleteItem(key[1]);
-      }),
-    ).then(async () => {
-      dispatch(allActions.main.clearAllData());
-      setShowConfirmPin(false);
-      resetStack(AuthenticationRouter.CREATE_NEW_WALLET);
-    });
-  };
-
   const resetStack = (name: string) => {
     reStack(StackName.AuthenticationStack, name);
   };
@@ -104,19 +58,8 @@ function SettingsScreen() {
         {listMenu.map((item, index) => {
           return <SettingMenuComponent data={item} key={index} />;
         })}
-        <CButton onPress={onDeleteAllData} style={styles.btnDelete}>
-          <Text style={styles.txtDelete}>Delete All Data</Text>
-        </CButton>
+        <DeleteAllDataButton />
       </Col>
-      {!showConfirmPin ? (
-        <CAlert ref={alertRef} onConfirm={onPressNext} />
-      ) : (
-        <CConfirmPinModal
-          isShow={showConfirmPin}
-          onConfirm={deleteAllData}
-          onCancel={() => setShowConfirmPin(false)}
-        />
-      )}
     </CLayout>
   );
 }
@@ -134,37 +77,5 @@ const styles = StyleSheet.create({
   title: {
     ...textStyles.Body1,
     color: colors.N2,
-  },
-  btnDelete: {
-    paddingVertical: scale(6),
-    paddingHorizontal: scale(16),
-    minWidth: scale(134),
-    height: scale(36),
-    borderRadius: scale(18),
-    borderWidth: scale(1),
-    borderColor: colors.N4,
-    alignSelf: 'center',
-    marginTop: scale(70),
-  },
-  txtDelete: {
-    ...textStyles.Body2,
-  },
-  messageContainer: {
-    flex: 1,
-    alignItems: 'center',
-    textAlign: 'center',
-  },
-  deleteMessage: {
-    textAlign: 'center',
-    fontSize: scale(16),
-    marginBottom: scale(20),
-    fontFamily: fonts.Lato.regular,
-  },
-  deleteQuestion: {
-    color: colors.R1,
-    fontWeight: '800',
-    fontSize: scale(20),
-    textAlign: 'center',
-    marginBottom: scale(20),
   },
 });
